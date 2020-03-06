7e7e1ae2c003120f5dfa1fafbc2f46fd8df524e7
Czy <651525974@qq.com>
2020-03-06 17:03:43 +0800

    Read More

--- 自动展开阅读全文.js
+++ 自动展开阅读全文.js
@@ -5,14 +5,21 @@
 // @description  try to take over the world!
 // @author       Czy
 // @match        https://blog.csdn.net/*
+// @match        https://www.zhihu.com/*
 // @grant        none
 // ==/UserScript==
+
+function show(dom){
+    dom[0] ? dom[0].click() : "";
+}
 
 (function() {
     'use strict';
 
-    /* CSDN */
-    document.getElementsByClassName('btn-readmore')[0] ? document.getElementsByClassName('btn-readmore')[0].click() : "";
+    /* CSDN 展开阅读全文 */
+    show(document.getElementsByClassName('btn-readmore'));
 
+    /* 知乎 查看全部回答 */
+    setTimeout(()=>{show(document.getElementsByClassName('QuestionMainAction ViewAll-QuestionMainAction'))},1000);
 
 })();
