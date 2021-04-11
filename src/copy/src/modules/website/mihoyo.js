3c74886fa3f8a1a0b63f3a69a9ffdf3f06656381
Czy <651525974@qq.com>
2021-04-11 09:18:56 +0800

    copy plugin add mihoyo

---
+++ src/copy/src/modules/website/mihoyo.js
@@ -0,0 +1,20 @@
+import common from "./common.js";
+
+const website = {
+    regexp: new RegExp(".+bbs.mihoyo.com/ys/obc.+"),
+    init: function($) {
+        common.hideButton($);
+        $(".detail__content").on("copy", e => e.stopPropagation());
+        const template = `
+            <style>
+                body{
+                    user-select: auto;
+                    -webkit-user-select: auto;
+                }
+            </style>
+        `;
+        $("body").append(template.replace(/\s*/, " "));
+    }
+} 
+
+export default website;
