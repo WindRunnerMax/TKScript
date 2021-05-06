2ebeeb558f1fe613a195c380472bfb5f679e7ea3
Czy <651525974@qq.com>
2021-05-06 18:15:21 +0800

    meta

---
+++ dist/meta/copy.meta.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @name        🔥🔥🔥文本选中复制🔥🔥🔥
+// @description 解除网站不允许复制的限制，文本选中后点击复制按钮即可复制，主要用于 百度文库 道客巴巴 无忧考网 学习啦 蓬勃范文 思否社区 力扣 知乎 语雀 等
+// @namespace   https://github.com/WindrunnerMax/TKScript
+// @version     2.2.1
+// @author      Czy
+// @include     *://wenku.baidu.com/view/*
+// @include     *://wenku.baidu.com/link*
+// @include     *://www.51test.net/show/*
+// @include     *://www.xuexi.la/*
+// @include     *://www.xuexila.com/*
+// @include     *://www.cspengbo.com/*
+// @include     *://*.doc88.com/*
+// @include     *://segmentfault.com/*
+// @include     *://wk.baidu.com/view/*
+// @include     *://leetcode-cn.com/problems/*
+// @include     *://www.zhihu.com/*
+// @include     *://z.30edu.com.cn/*
+// @include     *://docs.qq.com/doc/*
+// @include     *://boke112.com/post/*
+// @include     *://www.yuque.com/*
+// @include     *://www.commandlinux.com/*
+// @include     *://*.diyifanwen.com/*
+// @include     *://*.mbalib.com/*
+// @include     *://*.cnitpm.com/*
+// @include     *://bbs.mihoyo.com/ys/obc/*
+// @include     *://www.ruiwen.com/*
+// @license     GPL License
+// @require     https://cdn.bootcss.com/jquery/2.1.2/jquery.min.js
+// @require     https://cdn.jsdelivr.net/npm/clipboard@2/dist/clipboard.min.js
+// @connect     static.doc88.com
+// @grant       unsafeWindow
+// @grant       GM_xmlhttpRequest
+// ==/UserScript==
