f8ac9ccf67a31cbcee32896dcd3cbf57f7789673
Czy <651525974@qq.com>
2021-10-14 09:33:45 +0800

    feat: greasyfork-405130 discussions-101287

--- dist/copy.js
+++ dist/copy.js
@@ -2,7 +2,7 @@
 // @name        🔥🔥🔥文本选中复制🔥🔥🔥
 // @description 解除网站不允许复制的限制，文本选中后点击复制按钮即可复制，主要用于 百度文库 道客巴巴 无忧考网 学习啦 蓬勃范文 思否社区 力扣 知乎 语雀 等
 // @namespace   https://github.com/WindrunnerMax/TKScript
-// @version     2.2.9
+// @version     2.2.10
 // @author      Czy
 // @include     *://wenku.baidu.com/view/*
 // @include     *://wenku.baidu.com/link*
@@ -32,6 +32,7 @@
 // @include     *://www.wenxm.cn/*
 // @include     *://www.unjs.com/*
 // @include     *://www.ahsrst.cn/*
+// @include     *://*.yjbys.com/*
 // @supportURL  https://github.com/WindrunnerMax/TKScript/issues
 // @license     GPL License
 // @installURL  https://github.com/WindrunnerMax/TKScript
@@ -154,7 +155,7 @@
   };
 
   var website$2 = {
-    regexp: new RegExp("commandlinux|cnki|leetcode-cn|ruiwen|oh100|fwsir|wenxm|unjs|ahsrst"),
+    regexp: new RegExp("commandlinux|cnki|leetcode-cn|ruiwen|oh100|fwsir|wenxm|unjs|ahsrst|yjbys"),
     init: function init($) {
       $("body").append("<style id=\"copy-hide\">#_copy{display: none !important;}</style>");
     },
--- src/copy/src/modules/common.js
+++ src/copy/src/modules/common.js
@@ -1,6 +1,6 @@
 
 const website = {
-    regexp: new RegExp("commandlinux|cnki|leetcode-cn|ruiwen|oh100|fwsir|wenxm|unjs|ahsrst"),
+    regexp: new RegExp("commandlinux|cnki|leetcode-cn|ruiwen|oh100|fwsir|wenxm|unjs|ahsrst|yjbys"),
     init: function($) {
         $("body").append(`<style id="copy-hide">#_copy{display: none !important;}</style>`);
     },
