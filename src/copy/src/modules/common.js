7fcd33e72313766ad1eab550b0782351c0cf5383
Czy <651525974@qq.com>
2021-10-28 09:39:10 +0800

    feat: discussions-102388 360doc

--- src/copy/src/modules/common.js
+++ src/copy/src/modules/common.js
@@ -1,7 +1,7 @@
 import utils from "../utils";
 
 const website = {
-    regexp: new RegExp("commandlinux|cnki|leetcode-cn|ruiwen|oh100|fwsir|wenxm|unjs|ahsrst|yjbys"),
+    regexp: new RegExp("commandlinux|cnki|leetcode-cn|ruiwen|oh100|fwsir|wenxm|unjs|ahsrst|yjbys|360doc"),
     init: function($) {
         utils.hideButton($);
     },
