681c16b92ca496ef82d562093196937c0786d0cb
Czy <651525974@qq.com>
2021-12-28 20:18:16 +0800

    baiduwk-hook

--- src/copy/src/modules/baiduwk-s.ts
+++ src/copy/src/modules/baiduwk-s.ts
@@ -1,3 +1,24 @@
 // `tips`
 // /static/ndpcwenku/static/ndview/js/common/components.{hash}.js selectedTextTrim
 // button: search translate
+
+// `window.webpackJson Hook` `document-start` `grant connect`
+/*
+unsafeWindow.webpackJsonp = [];
+unsafeWindow.webpackJsonp.push = () => {};
+setTimeout(() => {
+    delete unsafeWindow.webpackJsonp.push;
+    window.$.getScript("https://wkstatic.bdimg.com/static/ndpcwenku/static/ndview/js/views/creader.7eb11c074c703be69e06.js", () => {
+        window.$.getScript("https://wkstatic.bdimg.com/static/ndpcwenku/static/ndview/js/common/common.a1efaf8d33ff1572ad92.js", () => {
+            GM_xmlhttpRequest({
+                method: "GET",
+                url: "https://wkstatic.bdimg.com/static/ndpcwenku/static/ndview/js/common/components.dea3c4d6a0375ab9beb1.js",
+                onload: function (response) {
+                    const code = response.responseText.replace("watch:{selectedTextTrim:function(t){", "watch:{selectedTextTrim:function(t){window.selectedTextTrim=t;")
+                    new Function(code)();
+                },
+            });
+        });
+    });
+},3000)
+*/
