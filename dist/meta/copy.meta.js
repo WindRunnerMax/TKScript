4efd52fa5b3dbf97f6899c1e924f86e2207ece7e
WindrunnerMax <651525974@qq.com>
2022-03-12 16:33:18 +0800

    update version

--- dist/meta/copy.meta.js
+++ dist/meta/copy.meta.js
@@ -2,7 +2,7 @@
 // @name        🔥🔥🔥文本选中复制🔥🔥🔥
 // @description 解除网站不允许复制的限制，文本选中后点击复制按钮即可复制，主要用于 百度文库 道客巴巴 无忧考网 学习啦 蓬勃范文 思否社区 力扣 知乎 语雀 等
 // @namespace   https://github.com/WindrunnerMax/TKScript
-// @version     3.1.7
+// @version     3.1.8
 // @author      Czy
 // @include     *://wenku.baidu.com/view/*
 // @include     *://wenku.baidu.com/share/*
