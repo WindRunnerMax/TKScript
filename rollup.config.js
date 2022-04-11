32c5c0c0f3536826ca1616ddcf79717e1247c18a
Czy <651525974@qq.com>
2022-04-11 10:02:33 +0800

    simplify packaging configuration

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,7 @@ import metablock from "rollup-plugin-userscript-metablock";
 import ts from "rollup-plugin-typescript2";
 import path from "path";
 
-const config = {
+const buildConfig = {
     postcss: {
         minimize: true,
         extensions: [".css"],
@@ -24,111 +24,73 @@ const config = {
     },
 };
 
-const metas = {
-    copy: {
-        input: "./meta/blank.js",
-        output: {
-            file: "./dist/meta/copy.meta.js",
-            format: "es",
-            name: "copyMetaModule",
+const scriptConfig = [
+    {
+        name: "Copy",
+        meta: {
+            input: "./meta/blank.js",
+            output: "./dist/meta/copy.meta.js",
+            metaFile: "./src/copy/meta.json",
         },
-        plugins: [
-            metablock({
-                file: "./src/copy/meta.json",
-            }),
-        ],
-    },
-    copyCurrency: {
-        input: "./meta/blank.js",
-        output: {
-            file: "./dist/meta/copy-currency.meta.js",
-            format: "es",
-            name: "copyMetaModule",
+        script: {
+            input: "./src/copy/src/index.ts",
+            output: "./dist/copy.user.js",
         },
-        plugins: [
-            metablock({
-                file: "./src/copy-currency/meta.json",
-            }),
-        ],
     },
-    siteDirector: {
-        input: "./meta/blank.js",
-        output: {
-            file: "./dist/meta/site-director.meta.js",
-            format: "es",
-            name: "copyMetaModule",
+    {
+        name: "CopyCurrency",
+        meta: {
+            input: "./meta/blank.js",
+            output: "./dist/meta/copy-currency.meta.js",
+            metaFile: "./src/copy-currency/meta.json",
         },
-        plugins: [
-            metablock({
-                file: "./src/site-director/meta.json",
-            }),
-        ],
+        script: {
+            input: "./src/copy-currency/src/index.ts",
+            output: "./dist/copy-currency.user.js",
+        },
     },
-};
+    {
+        name: "SiteDirector",
+        meta: {
+            input: "./meta/blank.js",
+            output: "./dist/meta/site-director.meta.js",
+            metaFile: "./src/site-director/meta.json",
+        },
+        script: {
+            input: "./src/site-director/src/index.ts",
+            output: "./dist/site-director.user.js",
+        },
+    },
+];
 
-const scripts = {
-    copy: {
-        input: "./src/copy/src/index.ts",
+export default [
+    ...scriptConfig.map(item => ({
+        input: item.meta.input,
         output: {
-            file: "./dist/copy.user.js",
+            file: item.meta.output,
+            format: "es",
+            name: item.name + "Meta",
+        },
+        plugins: [metablock({ file: item.meta.metaFile })],
+    })),
+    ...scriptConfig.map(item => ({
+        input: item.script.input,
+        output: {
+            file: item.script.output,
             format: "iife",
-            name: "copyModule",
+            name: item.name + "Module",
         },
         plugins: [
-            postcss(config.postcss),
-            babel(config.babel),
+            postcss(buildConfig.postcss),
+            babel(buildConfig.babel),
             ts({
-                tsconfig: path.resolve(__dirname, "./tsconfig.json"),
+                tsconfig: path.resolve(__dirname, "tsconfig.json"),
                 extensions: [".ts"],
             }),
             // uglify(),
-            metablock({
-                file: "./src/copy/meta.json",
-            }),
+            metablock({ file: item.meta.metaFile }),
         ],
-    },
-    copyCurrency: {
-        input: "./src/copy-currency/src/index.ts",
-        output: {
-            file: "./dist/copy-currency.user.js",
-            format: "iife",
-            name: "copyModule",
-        },
-        plugins: [
-            postcss(config.postcss),
-            babel(config.babel),
-            ts({
-                tsconfig: path.resolve(__dirname, "./tsconfig.json"),
-                extensions: [".ts"],
-            }),
-            // uglify(),
-            metablock({
-                file: "./src/copy-currency/meta.json",
-            }),
-        ],
-    },
-    siteDirector: {
-        input: "./src/site-director/src/index.ts",
-        output: {
-            file: "./dist/site-director.user.js",
-            format: "iife",
-            name: "linkModule",
-        },
-        plugins: [
-            postcss(config.postcss),
-            babel(config.babel),
-            ts({
-                tsconfig: path.resolve(__dirname, "./tsconfig.json"),
-                extensions: [".ts"],
-            }),
-            // uglify(),
-            metablock({
-                file: "./src/site-director/meta.json",
-            }),
-        ],
-    },
-};
-
-export default [...Object.values(metas), ...Object.values(scripts)];
+    })),
+];
 
 // https://segmentfault.com/a/1190000010628352
