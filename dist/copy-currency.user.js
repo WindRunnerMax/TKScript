8f035b539300ea755b16e52690a84f4100fa0e08
WindrunnerMax <651525974@qq.com>
2022-04-16 18:12:23 +0800

    copy-currency add en version

--- dist/copy-currency.user.js
+++ dist/copy-currency.user.js
@@ -1,8 +1,10 @@
 // ==UserScript==
-// @name    🔥🔥🔥文本选中复制(通用)🔥🔥🔥
-// @name:en Text Copy Universal
-// @description    文本选中复制通用版本，适用于大多数网站
-// @description:en Text copy general version, suitable for most websites.
+// @name       🔥🔥🔥文本选中复制(通用)🔥🔥🔥
+// @name:en    Text Copy Universal
+// @name:zh-CN 🔥🔥🔥文本选中复制(通用)🔥🔥🔥
+// @description       文本选中复制通用版本，适用于大多数网站
+// @description:en    Text copy general version, suitable for most websites.
+// @description:zh-CN 文本选中复制通用版本，适用于大多数网站
 // @namespace   https://github.com/WindrunnerMax/TKScript
 // @version     1.0.2
 // @author      Czy
