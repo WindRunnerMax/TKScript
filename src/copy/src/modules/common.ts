102553e44f31b5241c4d598ea117abbee98d0af1
蔡朝阳 <caizhaoyang.666@bytedance.com>
2022-06-14 09:51:45 +0800

    feat issue #41

--- src/copy/src/modules/common.ts
+++ src/copy/src/modules/common.ts
@@ -24,6 +24,7 @@ const website: Website = {
             "kodiplayer",
             "tongxiehui",
             "ndPureView",
+            "jianshu",
         ].join("|")
     ),
     init: function ($) {
