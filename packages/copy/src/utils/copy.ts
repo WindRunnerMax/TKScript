54f27685f24969aa63b40c573427648e2bae6a8e
WindrunnerMax <651525974@qq.com>
2022-10-06 20:08:30 +0800

    fix: firefox ClipboardItem

--- packages/copy/src/utils/copy.ts
+++ packages/copy/src/utils/copy.ts
@@ -1,3 +1,4 @@
+import { COPY } from "../constant/constant";
 import { isString } from "./is";
 
 type CopyData = {
@@ -10,12 +11,23 @@ export type CopyParams = CopyData | string;
 export const TEXT_PLAIN = "text/plain";
 export const TEXT_HTML = "text/html";
 
-const downgradeCopy = (text: string) => {
+const downgradeCopy = (data: CopyData) => {
     const textarea = document.createElement("textarea");
+    textarea.addEventListener(
+        COPY,
+        event => {
+            for (const [key, value] of Object.entries(data)) {
+                event.clipboardData.setData(key, value);
+            }
+            event.stopPropagation();
+            event.preventDefault();
+        },
+        true
+    );
     textarea.style.position = "fixed";
     textarea.style.left = "-999px";
     textarea.style.top = "-999px";
-    textarea.value = text;
+    textarea.value = data[TEXT_PLAIN];
     document.body.appendChild(textarea);
     textarea.select();
     document.execCommand("copy");
@@ -30,17 +42,17 @@ export const copy = (data: CopyParams): boolean => {
     const params: CopyData = isString(data) ? { [TEXT_PLAIN]: data } : data;
     const plainText = params[TEXT_PLAIN];
     if (!plainText) return false;
-    if (navigator.clipboard) {
+    if (navigator.clipboard && window.ClipboardItem) {
         const dataItems: Record<string, Blob> = {};
         for (const [key, value] of Object.entries(params)) {
             const blob = new Blob([value], { type: key });
             dataItems[key] = blob;
         }
         navigator.clipboard.write([new ClipboardItem(dataItems)]).catch(() => {
-            downgradeCopy(plainText);
+            downgradeCopy(params);
         });
     } else {
-        downgradeCopy(plainText);
+        downgradeCopy(params);
     }
     return true;
 };
