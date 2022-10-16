0dfce10433bc7f14fe43badb69256623286c1cb9
WindrunnerMax <651525974@qq.com>
2022-10-16 01:46:37 +0000

    Deploying to gh-pages from  @ 4228ee1cc0afa1e4f51bf1c0577328d718646d76 🚀

--- copy.user.js
+++ copy.user.js
@@ -144,6 +144,10 @@
       });
     };
 
+    /*!
+     * 外部引用`static.doc88.com`声明
+     * 此部分是在处理`doc88.com`才会加载的资源文件，此资源文件由该网站加载时提供
+     */
     let path = "";
     const website$r = {
       regexp: /.*doc88\.com\/.+/,
