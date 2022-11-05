81ee8a253ce912ac54a0918e23a30f7e4ddf0bb2
WindrunnerMax <651525974@qq.com>
2022-11-05 10:26:09 +0800

    feat: debug mode

---
+++ meta/debug.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name       DEBUG
+// @description       DEBUG MODE
+// @namespace  https://github.com/WindrunnerMax/TKScript
+// @version    1.0.0
+// @author     Czy
+// @match      http://*/*
+// @match      https://*/*
+// @license    GPL License
+// @run-at     document-end
+// @grant      GM_notification
+// @require    file:///Users/czy/Project/TKScript/dist/copy-currency.user.js
+// ==/UserScript==
