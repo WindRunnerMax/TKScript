a86285c4b84fb207bbde3ebf9467d7238ef8729d
WindrunnerMax <651525974@qq.com>
2022-12-02 11:18:43 +0000

    Deploying to gh-pages from  @ 8ab010d43bbf2387e50ff16bc04b66b6b920d67e 🚀

--- copy.user.js
+++ copy.user.js
@@ -2,7 +2,7 @@
 // @name        🔥🔥🔥文本选中复制🔥🔥🔥
 // @description 解除网站不允许复制的限制，文本选中后点击复制按钮即可复制，主要用于 百度文库 道客巴巴 腾讯文档 豆丁网 无忧考网 学习啦 蓬勃范文 思否社区 力扣 知乎 语雀 等
 // @namespace   https://github.com/WindrunnerMax/TKScript
-// @version     6.1.4
+// @version     6.1.5
 // @author      Czy
 // @match       *://wenku.baidu.com/view/*
 // @match       *://wenku.baidu.com/share/*
@@ -74,6 +74,8 @@
 // @match       *://*.docin.com/*
 // @match       *://*.ddwk8.cn/*
 // @match       *://*.php.cn/*
+// @match       *://cooco.net.cn/*
+// @match       *://fanqienovel.com/*
 // @supportURL  https://github.com/WindrunnerMax/TKScript/issues
 // @license     GPL License
 // @installURL  https://github.com/WindrunnerMax/TKScript
@@ -778,7 +780,9 @@
           "ximalaya",
           "xiexiebang",
           "ddwk8",
-          "php\\.cn"
+          "php\\.cn",
+          "fanqienovel\\.com/reader",
+          "cooco\\.net\\.cn"
         ].join("|")
       ),
       init: function() {
