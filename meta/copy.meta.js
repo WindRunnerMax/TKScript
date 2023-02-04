8c1d3c590f77eb95193d8acd6228db8b75f8558b
WindrunnerMax <651525974@qq.com>
2023-02-04 02:26:31 +0000

    Deploying to gh-pages from  @ 3ca6a30f787dae3e6331dcfb65706f5d2d6d7eb1 🚀

--- meta/copy.meta.js
+++ meta/copy.meta.js
@@ -2,7 +2,7 @@
 // @name        🔥🔥🔥文本选中复制🔥🔥🔥
 // @description 解除网站不允许复制的限制，文本选中后点击复制按钮即可复制，主要用于 百度文库 道客巴巴 腾讯文档 豆丁网 无忧考网 学习啦 蓬勃范文 思否社区 力扣 知乎 语雀 等
 // @namespace   https://github.com/WindrunnerMax/TKScript
-// @version     6.1.8
+// @version     6.1.9
 // @author      Czy
 // @match       *://wenku.baidu.com/view/*
 // @match       *://wenku.baidu.com/share/*
@@ -78,6 +78,8 @@
 // @match       *://fanqienovel.com/*
 // @match       *://*.mobiletrain.org/*
 // @match       *://*.examcoo.com/*
+// @match       *://*.rrdynb.com/*
+// @match       *://*.fuwu7.com/*
 // @supportURL  https://github.com/WindrunnerMax/TKScript/issues
 // @license     GPL License
 // @installURL  https://github.com/WindrunnerMax/TKScript
