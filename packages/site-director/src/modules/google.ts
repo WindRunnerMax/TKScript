e1847feff9342c224591996e03b724b39d63dfb0
WindrunnerMax <651525974@qq.com>
2023-02-26 12:52:11 +0800

    feat: site-director scholar.google

--- packages/site-director/src/modules/google.ts
+++ packages/site-director/src/modules/google.ts
@@ -3,9 +3,11 @@ import { Website } from "../websites";
 const website: Website = {
     regexp: /google/,
     init: function () {
+        const isScholar = window.location.host.startsWith("scholar");
+
         document.addEventListener("DOMContentLoaded", () =>
             document
-                .querySelectorAll("#res a")
+                .querySelectorAll(isScholar ? "#gs_bdy_ccl a" : "#res a")
                 .forEach(item => item.setAttribute("target", "_blank"))
         );
     },
