2b2ff5a0e336488d52da239831fe17328ae2df93
WindrunnerMax <651525974@qq.com>
2023-09-26 21:45:26 +0800

    feat: pnpm workspace

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,37 +1,37 @@
 module.exports = {
-    extends: ["eslint:recommended", "plugin:prettier/recommended"],
-    env: {
-        browser: true,
-        node: true,
-        commonjs: true,
-        es2021: true,
+  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  env: {
+    browser: true,
+    node: true,
+    commonjs: true,
+    es2021: true,
+  },
+  parserOptions: {
+    requireConfigFile: false,
+    ecmaVersion: 2020,
+    sourceType: "module",
+  },
+  overrides: [
+    {
+      files: ["*.ts"],
+      parser: "@typescript-eslint/parser",
+      plugins: ["@typescript-eslint"],
+      extends: ["plugin:@typescript-eslint/recommended"],
     },
-    parserOptions: {
-        requireConfigFile: false,
-        ecmaVersion: 2020,
-        sourceType: "module",
-    },
-    overrides: [
-        {
-            files: ["*.ts"],
-            parser: "@typescript-eslint/parser",
-            plugins: ["@typescript-eslint"],
-            extends: ["plugin:@typescript-eslint/recommended"],
-        },
-    ],
-    ignorePatterns: ["node_modules", "build", "dist", "coverage"],
-    rules: {
-        // 分号
-        "semi": "error",
-        // 对象键值引号样式保持一致
-        "quote-props": ["error", "consistent-as-needed"],
-        // 箭头函数允许单参数不带括号
-        "arrow-parens": ["error", "as-needed"],
-        // no var
-        "no-var": "error",
-        // const
-        "prefer-const": "error",
-        // 允许console
-        "no-console": "off",
-    },
+  ],
+  ignorePatterns: ["node_modules", "build", "dist", "coverage"],
+  rules: {
+    // 分号
+    "semi": "error",
+    // 对象键值引号样式保持一致
+    "quote-props": ["error", "consistent-as-needed"],
+    // 箭头函数允许单参数不带括号
+    "arrow-parens": ["error", "as-needed"],
+    // no var
+    "no-var": "error",
+    // const
+    "prefer-const": "error",
+    // 允许console
+    "no-console": "off",
+  },
 };
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,6 @@
 module.exports = {
     "printWidth": 100, // 指定代码长度，超出换行
-    "tabWidth": 4, // tab 键的宽度
+    "tabWidth": 2, // tab 键的宽度
     "useTabs": false, // 不使用tab
     "semi": true, // 结尾加上分号
     "singleQuote": false, // 使用单引号
