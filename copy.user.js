c7b30ae84dd82165334d507e6e61d4eec17036a2
WindrunnerMax <651525974@qq.com>
2023-10-21 03:29:59 +0000

    Deploying to gh-pages from  @ b943e7c239cc5835ebdaed311ddafccbecfd2891 🚀

--- copy.user.js
+++ copy.user.js
@@ -2,7 +2,7 @@
 // @name        🔥🔥🔥文本选中复制🔥🔥🔥
 // @description 解除网站不允许复制的限制，文本选中后点击复制按钮即可复制，主要用于 百度文库 道客巴巴 腾讯文档 豆丁网 无忧考网 学习啦 蓬勃范文 思否社区 力扣 知乎 语雀 等
 // @namespace   https://github.com/WindrunnerMax/TKScript
-// @version     6.1.21
+// @version     6.1.22
 // @author      Czy
 // @match       *://wenku.baidu.com/view/*
 // @match       *://wenku.baidu.com/share/*
@@ -461,6 +461,7 @@
         };
       },
       getSelectedText: function() {
+        var _a;
         if (unsafeWindow.pad && unsafeWindow.pad.editor && !unsafeWindow.pad.editor.isCopyable()) {
           utils.showButton();
           const editor = unsafeWindow.pad.editor;
@@ -495,7 +496,7 @@
                 const cell = SpreadsheetApp.workbook.activeSheet.getCellDataAtPosition(i, k);
                 if (!cell)
                   continue;
-                text.push(" ", cell.value || "");
+                text.push(" ", ((_a = cell.formattedValue) == null ? void 0 : _a.value) || cell.value || "");
               }
               i !== endRowIndex && text.push("\n");
             }
