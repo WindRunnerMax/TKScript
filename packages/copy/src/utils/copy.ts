b1916499231aa84d8d373e5345d4b9523b8cd75b
WindrunnerMax <651525974@qq.com>
2024-07-06 10:04:30 +0800

    fix: possible memory leaks

--- packages/copy/src/utils/copy.ts
+++ packages/copy/src/utils/copy.ts
@@ -11,26 +11,24 @@ export type CopyParams = CopyData | string;
 export const TEXT_PLAIN = "text/plain";
 export const TEXT_HTML = "text/html";
 
-const downgradeCopy = (data: CopyData) => {
+const execCopyCommand = (data: CopyData) => {
   const textarea = document.createElement("textarea");
-  textarea.addEventListener(
-    COPY,
-    event => {
-      for (const [key, value] of Object.entries(data)) {
-        event.clipboardData && event.clipboardData.setData(key, value);
-      }
-      event.stopPropagation();
-      event.preventDefault();
-    },
-    true
-  );
+  const handler = (event: ClipboardEvent) => {
+    event.preventDefault();
+    event.stopImmediatePropagation();
+    for (const [key, value] of Object.entries(data)) {
+      event.clipboardData && event.clipboardData.setData(key, value);
+    }
+  };
+  textarea.addEventListener(COPY, handler, true);
   textarea.style.position = "fixed";
-  textarea.style.left = "-999px";
-  textarea.style.top = "-999px";
-  textarea.value = data[TEXT_PLAIN];
+  textarea.style.left = "-999999999px";
+  textarea.style.top = "-999999999px";
+  textarea.value = data[TEXT_PLAIN] || " ";
   document.body.appendChild(textarea);
   textarea.select();
   document.execCommand("copy");
+  textarea.removeEventListener(COPY, handler);
   document.body.removeChild(textarea);
 };
 
@@ -50,10 +48,10 @@ export const copy = (data: CopyParams): boolean => {
       dataItems[key] = blob;
     }
     navigator.clipboard.write([new ClipboardItem(dataItems)]).catch(() => {
-      downgradeCopy(params);
+      execCopyCommand(params);
     });
   } else {
-    downgradeCopy(params);
+    execCopyCommand(params);
   }
   return true;
 };
