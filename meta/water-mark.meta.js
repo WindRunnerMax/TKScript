325bcd6ae5a37f7ed9854895553f5644af7b8086
WindRunnerMax <651525974@qq.com>
2024-11-12 15:30:51 +0000

    Deploying to gh-pages from  @ eb9254fb9962038ac0ffe4d22ed4158c2208646f 🚀

--- meta/water-mark.meta.js
+++ meta/water-mark.meta.js
@@ -2,11 +2,9 @@
 // @name        移除页面水印
 // @description 移除常见网页的水印
 // @namespace   https://github.com/WindrunnerMax/TKScript
-// @version     1.0.0
+// @version     1.0.1
 // @author      Czy
 // @match       *://so.csdn.net/*
-// @match       *://*.feishu.cn/*
-// @match       *://*.larkoffice.com/*
 // @supportURL  https://github.com/WindrunnerMax/TKScript/issues
 // @license     GPL License
 // @installURL  https://github.com/WindrunnerMax/TKScript
--- water-mark.user.js
+++ water-mark.user.js
@@ -2,11 +2,9 @@
 // @name        移除页面水印
 // @description 移除常见网页的水印
 // @namespace   https://github.com/WindrunnerMax/TKScript
-// @version     1.0.0
+// @version     1.0.1
 // @author      Czy
 // @match       *://so.csdn.net/*
-// @match       *://*.feishu.cn/*
-// @match       *://*.larkoffice.com/*
 // @supportURL  https://github.com/WindrunnerMax/TKScript/issues
 // @license     GPL License
 // @installURL  https://github.com/WindrunnerMax/TKScript
@@ -18,10 +16,29 @@
 
   const FALLBACK_CLASS = "_watermark";
   const OPACITY_PROPERTY = [
-    "opacity:0 !important;",
-    "pointer-events:none !important;",
-    "visibility:hidden !important;"
+    "opacity: 0 !important;",
+    "visibility: hidden !important;",
+    "transform: translate(-999999px, -999999px) !important;"
   ].join("");
+  const OPACITY_BACKGROUND_PROPERTY = [
+    "background: transparent !important;",
+    "background-color: transparent !important;",
+    "background-image: linear-gradient(to right, rgba(255, 255, 255, 0), rgba(255, 255, 255, 0)) !important;"
+  ].join("");
+
+  const lintWaterMarkDOM = (node) => {
+    if (node instanceof Element && node.hasAttribute("style")) {
+      const styles = node.getAttribute("style") || "";
+      if (styles.indexOf("pointer-events: none;") === -1) {
+        return false;
+      }
+      if (styles.indexOf("background: url") > -1 || styles.indexOf("background-image: url") > -1) {
+        !node.classList.contains(FALLBACK_CLASS) && node.classList.add(FALLBACK_CLASS);
+        return true;
+      }
+    }
+    return false;
+  };
 
   const styles = {
     insertCSS: (id, css) => {
@@ -57,6 +74,46 @@
     styles.insertCSS(String(Math.random()), css);
   };
 
+  const common = {
+    regexp: /.*/,
+    init: () => {
+      const observer = MutationObserver.prototype.observe;
+      MutationObserver.prototype.observe = function(target, options) {
+        lintWaterMarkDOM(target);
+        target.childNodes.forEach(lintWaterMarkDOM);
+        observer.call(this, target, options);
+      };
+      const _MutationObserver = MutationObserver;
+      const cb = (callback) => {
+        return (records, observer2) => {
+          let isMatchedWaterMarkDOM = false;
+          for (const record of records) {
+            if (lintWaterMarkDOM(record.target) && !isMatchedWaterMarkDOM) {
+              isMatchedWaterMarkDOM = true;
+            }
+          }
+          !isMatchedWaterMarkDOM && callback(records, observer2);
+        };
+      };
+      unsafeWindow.MutationObserver = class extends _MutationObserver {
+        constructor(callback) {
+          super(cb(callback));
+        }
+      };
+      const PRESET_CLASSES = [
+        "." + FALLBACK_CLASS,
+        `[class*="watermark"]`,
+        `[class*="WaterMark"]`
+      ].join(",");
+      injectCSSEarly(`${PRESET_CLASSES}{${OPACITY_PROPERTY}}`);
+      const PRESET_BACKGROUND = [
+        `[style*="pointer-events: none;"][style*="background: url"]`,
+        `[style*="pointer-events: none;"][style*="background-image: url"]`
+      ].join(",");
+      injectCSSEarly(`${PRESET_BACKGROUND}{${OPACITY_BACKGROUND_PROPERTY}}`);
+    }
+  };
+
   const csdn = {
     regexp: /so\.csdn\.net/,
     init: () => {
@@ -64,12 +121,7 @@
       MutationObserver.prototype.observe = function(target, options) {
         if (target instanceof Element && target.classList.contains("chat-mask")) {
           const nodes = Array.from(target.children);
-          nodes.forEach((node) => {
-            const styles = node.getAttribute("style") || "";
-            if (styles.indexOf("data:image/") > -1 && styles.indexOf("background-image") > -1 && !node.classList.contains(FALLBACK_CLASS)) {
-              node.classList.add(FALLBACK_CLASS);
-            }
-          });
+          nodes.forEach((node) => lintWaterMarkDOM(node));
           return;
         }
         observer.call(this, target, options);
@@ -79,41 +131,7 @@
     }
   };
 
-  const feishu = {
-    regexp: /(feishu\.cn)|(larkoffice\.com)/,
-    init: () => {
-      const _MutationObserver = MutationObserver;
-      const cb = (records) => {
-        records.forEach((r) => {
-          if (r.target instanceof Element && r.target.hasAttribute("style")) {
-            const el = r.target;
-            const styles = el.getAttribute("style") || "";
-            if (styles.indexOf("data:image/") > -1 && styles.indexOf("background-image") > -1 && !el.classList.contains(FALLBACK_CLASS)) {
-              el.classList.add(FALLBACK_CLASS);
-            }
-          }
-        });
-      };
-      unsafeWindow.MutationObserver = class extends _MutationObserver {
-        constructor(callback) {
-          const fn = callback.toString();
-          const isWatermark = /watermark/.test(fn);
-          super(isWatermark ? cb : callback);
-        }
-      };
-      const classes = [
-        "." + FALLBACK_CLASS,
-        ".token-watermark-bg",
-        ".ssrWaterMark",
-        ".ssrHiddenWaterMark",
-        ".print-watermark",
-        ".suite-clear"
-      ].join(",");
-      injectCSSEarly(`${classes}{${OPACITY_PROPERTY}}`);
-    }
-  };
-
-  const websites = [feishu, csdn];
+  const websites = [csdn, common];
   const web = websites.find((item) => item.regexp.test(location.href));
   web && web.init();
 
