6574e67c5e3f55f69c47bf221c8ddcb260599d80
WindRunnerMax <651525974@qq.com>
2024-11-15 19:12:20 +0800

    chore: exactly match

--- packages/water-mark/src/modules/common.ts
+++ packages/water-mark/src/modules/common.ts
@@ -31,19 +31,19 @@ export const common: Website = {
     };
     const PRESET_CLASSES = [
       "." + FALLBACK_CLASS,
-      `[id*="watermark"]`,
-      `[id*="WaterMark"]`,
-      `[id*="Watermark"]`,
-      `[class*="watermark"]`,
-      `[class*="WaterMark"]`,
-      `[class*="Watermark"]`,
+      `div[id*="watermark"]`,
+      `div[id*="WaterMark"]`,
+      `div[id*="Watermark"]`,
+      `div[class*="watermark"]`,
+      `div[class*="WaterMark"]`,
+      `div[class*="Watermark"]`,
     ].join(",");
     injectCSSEarly(`${PRESET_CLASSES}{${OPACITY_PROPERTY}}`);
     const PRESET_BACKGROUND = [
-      `[style*="pointer-events: none;"][style*="background: url"]`,
-      `[style*="pointer-events: none;"][style*="background-image: url"]`,
-      `[style*="pointer-events:none;"][style*="background:url"]`,
-      `[style*="pointer-events:none;"][style*="background-image:url"]`,
+      `div[style*="pointer-events"][style*="background: url"]`,
+      `div[style*="pointer-events"][style*="background-image: url"]`,
+      `div[style*="pointer-events"][style*="background:url"]`,
+      `div[style*="pointer-events"][style*="background-image:url"]`,
     ].join(",");
     injectCSSEarly(`${PRESET_BACKGROUND}{${OPACITY_BACKGROUND_PROPERTY}}`);
   },
