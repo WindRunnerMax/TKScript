b64c7645619625e2fb0f32e10668ddd6331a1daa
WindRunnerMax <651525974@qq.com>
2025-01-10 21:01:18 +0800

    feat: copy #144

--- packages/copy/src/modules/doc88.ts
+++ packages/copy/src/modules/doc88.ts
@@ -2,29 +2,21 @@ import dom from "../utils/dom";
 import type { Website } from "../websites";
 
 /*!
- * 外部引用`static.doc88.com`声明
- * 此部分是在处理`doc88.com`才会加载的资源文件，此资源文件由该网站加载时提供
+ * 外部引用 static.doc88.com 声明
+ * 此部分是在处理 doc88.com 才会加载的资源文件，此资源文件由该网站加载时提供
  */
 
 let path = "";
 const website: Website = {
   regexp: /.*doc88\.com\/.+/,
   init: () => {
-    // GM_xmlhttpRequest({
-    //     method: "GET",
-    //     url: "https://res.doc88.com/assets/js/v2.js",
-    //     onload: function(response) {
-    //         var view = new Function("var view = " + response.responseText.replace("eval", "") + "; return view;");
-    //         path = /<textarea[\s\S]*?Viewer.([\S]*?)\+[\S]*?\/textarea>/.exec(view())[1];
-    //     }
-    // })
     dom.append(
       "body",
       `<style id="copy-element-hide">#left-menu{display: none !important;}</style>`
     );
     GM_xmlhttpRequest({
       method: "GET",
-      url: "https://res3.doc88.com/resources/js/modules/main-v2.min.js?v=2.56",
+      url: "https://res3.doc88.com/resources/js/modules/main-v2.min.js?v=3.55",
       onload: function (response) {
         const result = /\("#cp_textarea"\).val\(([\S]*?)\);/.exec(response.responseText);
         if (result) path = result[1];
