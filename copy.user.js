bd14b783a9135096b81f7c809ca2e19e57de5d0c
WindRunnerMax <651525974@qq.com>
2026-01-03 12:05:59 +0000

    Deploying to gh-pages from  @ 7a4e85426f6c60cf6de5c6705a45eca7a698cc2e 🚀

--- copy.user.js
+++ copy.user.js
@@ -2,7 +2,7 @@
 // @name        🔥🔥🔥文本选中复制🔥🔥🔥
 // @description 解除网站不允许复制的限制，文本选中后点击复制按钮即可复制，主要用于 百度文库 道客巴巴 腾讯文档 豆丁网 无忧考网 学习啦 蓬勃范文 思否社区 力扣 知乎 语雀 等
 // @namespace   https://github.com/WindrunnerMax/TKScript
-// @version     6.2.9
+// @version     6.2.10
 // @author      Czy
 // @match       *://wenku.baidu.com/view/*
 // @match       *://wenku.baidu.com/share/*
@@ -500,7 +500,8 @@
             };
           }
         }
-        if (unsafeWindow.SpreadsheetApp && unsafeWindow.SpreadsheetApp.permissions && unsafeWindow.SpreadsheetApp.permissions.sheetStatus && unsafeWindow.SpreadsheetApp.permissions.sheetStatus.canCopy === false && unsafeWindow.SpreadsheetApp.permissions.sheetStatus.canEdit && unsafeWindow.SpreadsheetApp.permissions.sheetStatus.canEdit() === false) {
+        const sheetStatus = unsafeWindow.SpreadsheetApp ? unsafeWindow.SpreadsheetApp.permissions ? unsafeWindow.SpreadsheetApp.permissions.sheetStatus : unsafeWindow.SpreadsheetApp.sheetStatus : null;
+        if (sheetStatus && sheetStatus.canCopy === false && sheetStatus.canEdit && sheetStatus.canEdit() === false) {
           instance.enable();
           const SpreadsheetApp = unsafeWindow.SpreadsheetApp;
           const [selection] = SpreadsheetApp.view.getSelectionRanges();
